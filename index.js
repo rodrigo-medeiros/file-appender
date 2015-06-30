e8b99933f537b818301f76e6bb444be2986e8166
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-06-30 00:42:36 -0300

    Added some more tests.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ module.exports = appender;
 
 function appender (input) {
 
+  if (!(input = helper.init(input)).length) {
+    return;
+
+  }
+
   var destination = new FileAppender();
 
   return helper.pipeNext(input, destination);
--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var files = [
   path.join(dirPath, "file2.txt")
 ];
 
-describe("", function () {
+describe("When an array of valid files is passed", function () {
 
   var contents = "";
 
@@ -24,42 +24,115 @@ describe("", function () {
 
   });
 
-  describe("When an array of valid files is passed", function () {
+  it("should create a transform stream", function (done) {
 
-    it("should create a transform stream", function (done) {
+    var filesArr = [].slice.call(files);
+    var transformStream = appender(filesArr);
 
-      var filesArr = [].slice.call(files, 0);
-      var transformStream = appender(filesArr);
+    expect(transformStream instanceof stream.Transform).to.be(true);
+    done();
 
-      expect(transformStream instanceof stream.Transform).to.be(true);
-      done();
+  });
 
-    });
+  it("should create a file with the contents of the files passed if piped to a write stream", function (done) {
 
-    it("should create a file with the contents of the files passed if piped to a write stream", function (done) {
+    var filesArr = [].slice.call(files);
+    var transformStream = appender(filesArr);
+    var output = fs.createWriteStream(path.join(dirPath, 'output.txt'));
 
-      var filesArr = [].slice.call(files, 0);
-      var transformStream = appender(filesArr);
-      var output = fs.createWriteStream(path.join(dirPath, 'output.txt'));
+    transformStream 
+      .pipe(output)
+      .on('finish', function () {
 
-      transformStream 
-        .pipe(output)
-        .on('finish', function () {
+        fs.readFileAsync(path.join(dirPath, 'output.txt')).then(function (text) {
 
-          fs.readFileAsync(path.join(dirPath, 'output.txt')).then(function (text) {
+          expect(contents).to.be(text.toString());
+          done();
 
-            expect(contents).to.be(text.toString());
-            done();
+        }).catch(function (err) {
 
-          }).catch(function (err) {
-
-            done(err);
-
-          });
+          done(err);
 
         });
 
+      });
+
+  });
+
+});
+
+describe("When just one path is passed", function () {
+
+  var contents = "";
+
+  before(function (done) {
+
+    files.pop();
+
+    readFiles(files).then(function (contentsArr) {
+      contents = contentsArr.join('');
+      done();
     });
+
+  });
+  
+  it("should create a transform stream", function (done) {
+
+    var filePath = path.join(dirPath, "file1.txt");
+    var transformStream = appender(filePath);
+
+    expect(transformStream instanceof stream.Transform).to.be(true);
+    done();
+
+  });
+
+  it("should create a file with the contents of the file passed if piped to a write stream", function (done) {
+
+    var filesArr = [].slice.call(files);
+    var transformStream = appender(filesArr);
+    var output = fs.createWriteStream(path.join(dirPath, 'output.txt'));
+
+    transformStream 
+      .pipe(output)
+      .on('finish', function () {
+
+        fs.readFileAsync(path.join(dirPath, 'output.txt')).then(function (text) {
+
+          expect(contents).to.be(text.toString());
+          done();
+
+        }).catch(function (err) {
+
+          done(err);
+
+        });
+
+      });
+
+  });
+
+});
+
+describe("When invalid stuff is passed", function () {
+  
+  it("should throw an error for null", function (done) {
+
+    expect(appender).to.throwException();
+    done();
+
+  });
+
+  it("should return undefined for empty string", function (done) {
+
+    expect(appender("")).to.not.be.ok();
+    done();
+
+  });
+
+  it("should return undefined for empty array", function (done) {
+
+    expect(appender([])).to.not.be.ok();
+    done();
 
   });
 
